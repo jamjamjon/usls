607f86b937e9f942913cc3dc69f8f60f4e03715d
jamjamjon <51357717+jamjamjon@users.noreply.github.com>
2023-02-23 15:56:13 +0800

    Update __init__.py

--- usls/src/__init__.py
+++ usls/src/__init__.py
@@ -8,5 +8,6 @@ from .video_tools import play_and_record, video_to_images, videos_to_images, ima
 from .spider import spider_img_baidu
 from .dir_combine import dir_combine
 from .label_combine import combine_labels
+from .class_modify import class_modify
 
 
