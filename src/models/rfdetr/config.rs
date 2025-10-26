22d0df542d1084d9944c7c97d39836618b908941
Jamjamjon <51357717+jamjamjon@users.noreply.github.com>
2025-10-27 00:22:32 +0800

    Add RF-DETR-seg model (#154)
    Add RF-DETR models:
    - rf-detr-nano
    - rf-detr-small
    - rf-detr-medium
    - rf-detr-seg-preview

--- src/models/rfdetr/config.rs
+++ src/models/rfdetr/config.rs
@@ -1,6 +1,6 @@
-use crate::NAMES_COCO_91;
+use crate::{Task, NAMES_COCO_91, NAMES_OBJECT365_366};
 
-/// Model configuration for `RT-DETR`
+/// Model configuration for `RF-DETR`
 impl crate::Config {
     pub fn rfdetr() -> Self {
         Self::default()
@@ -13,13 +13,39 @@ impl crate::Config {
             .with_image_mean(&[0.485, 0.456, 0.406])
             .with_image_std(&[0.229, 0.224, 0.225])
             .with_class_names(&NAMES_COCO_91)
+            .with_task(Task::ObjectDetection)
+            .with_class_confs(&[0.3])
+    }
+
+    pub fn rfdetr_nano() -> Self {
+        Self::rfdetr().with_model_file("nano.onnx")
+    }
+
+    pub fn rfdetr_small() -> Self {
+        Self::rfdetr().with_model_file("small.onnx")
+    }
+
+    pub fn rfdetr_medium() -> Self {
+        Self::rfdetr().with_model_file("medium.onnx")
     }
 
     pub fn rfdetr_base() -> Self {
         Self::rfdetr().with_model_file("base.onnx")
     }
 
+    pub fn rfdetr_base_obj365() -> Self {
+        Self::rfdetr()
+            .with_class_names(&NAMES_OBJECT365_366)
+            .with_model_file("base-obj365.onnx")
+    }
+
     pub fn rfdetr_large() -> Self {
         Self::rfdetr().with_model_file("large.onnx")
+    }
+
+    pub fn rfdetr_seg_preview() -> Self {
+        Self::rfdetr()
+            .with_task(Task::InstanceSegmentation)
+            .with_model_file("seg-preview.onnx")
     }
 }
--- src/models/rfdetr/impl.rs
+++ src/models/rfdetr/impl.rs
@@ -3,7 +3,9 @@ use anyhow::Result;
 use ndarray::{s, Axis};
 use rayon::prelude::*;
 
-use crate::{elapsed_module, Config, DynConf, Engine, Hbb, Image, Processor, Xs, Y};
+use crate::{
+    elapsed_module, Config, DynConf, Engine, Hbb, Image, Mask, Ops, Processor, Task, Xs, Y,
+};
 
 #[derive(Debug, Builder)]
 pub struct RFDETR {
@@ -15,6 +17,7 @@ pub struct RFDETR {
     confs: DynConf,
     processor: Processor,
     spec: String,
+    task: Task,
 }
 
 impl RFDETR {
@@ -25,12 +28,14 @@ impl RFDETR {
             engine.try_height().unwrap_or(&560.into()).opt(),
             engine.try_width().unwrap_or(&560.into()).opt(),
         );
+        let task = config.task().unwrap_or(&Task::ObjectDetection).clone();
         let spec = engine.spec().to_owned();
         let names: Vec<String> = config.class_names().to_vec();
         let confs = DynConf::new_or_default(config.class_confs(), names.len());
         let processor = Processor::try_from_config(&config.processor)?
             .with_image_width(width as _)
             .with_image_height(height as _);
+
         Ok(Self {
             engine,
             height,
@@ -40,6 +45,7 @@ impl RFDETR {
             names,
             confs,
             processor,
+            task,
         })
     }
 
@@ -65,6 +71,11 @@ impl RFDETR {
     fn postprocess(&mut self, xs: Xs) -> Result<Vec<Y>> {
         // 0: bboxes
         // 1: logits
+        // 2: masks
+        if xs.len() > 2 {
+            self.task = Task::InstanceSegmentation;
+        }
+
         let ys: Vec<Y> = xs[1]
             .axis_iter(Axis(0))
             .into_par_iter()
@@ -75,7 +86,7 @@ impl RFDETR {
                     self.processor.images_transform_info[idx].width_src,
                 );
                 let ratio = self.processor.images_transform_info[idx].height_scale;
-                let y_bboxes: Vec<Hbb> = logits
+                let y_bboxes_masks: Vec<(Hbb, Option<Mask>)> = logits
                     .axis_iter(Axis(0))
                     .into_par_iter()
                     .enumerate()
@@ -84,7 +95,9 @@ impl RFDETR {
                             .mapv(|x| 1. / ((-x).exp() + 1.))
                             .iter()
                             .enumerate()
-                            .max_by(|a, b| a.1.total_cmp(b.1))?;
+                            .max_by(|a, b| {
+                                a.1.partial_cmp(b.1).unwrap_or(std::cmp::Ordering::Equal)
+                            })?;
 
                         if conf < self.confs[idx] {
                             return None;
@@ -107,14 +120,49 @@ impl RFDETR {
                             hbb = hbb.with_name(&self.names[class_id]);
                         }
 
-                        Some(hbb)
+                        if matches!(self.task, Task::InstanceSegmentation) {
+                            let mask = xs[2].slice(s![idx, i, .., ..]);
+                            let (mh, mw) = (mask.shape()[0], mask.shape()[1]);
+                            let mask = mask.into_owned().into_raw_vec_and_offset().0;
+                            let mask_resized = Ops::resize_lumaf32_u8(
+                                &mask,
+                                mw as _,
+                                mh as _,
+                                image_width as _,
+                                image_height as _,
+                                true,
+                                "Bilinear",
+                            )
+                            .ok()?;
+
+                            let mask_image: image::ImageBuffer<image::Luma<_>, Vec<_>> =
+                                image::ImageBuffer::from_raw(
+                                    image_width as _,
+                                    image_height as _,
+                                    mask_resized,
+                                )?;
+
+                            let mut mask = Mask::default().with_mask(mask_image);
+                            if let Some(id) = hbb.id() {
+                                mask = mask.with_id(id);
+                            }
+                            if let Some(name) = hbb.name() {
+                                mask = mask.with_name(name);
+                            }
+                            if let Some(confidence) = hbb.confidence() {
+                                mask = mask.with_confidence(confidence);
+                            }
+                            Some((hbb, Some(mask)))
+                        } else {
+                            Some((hbb, None))
+                        }
                     })
                     .collect();
 
-                let mut y = Y::default();
-                if !y_bboxes.is_empty() {
-                    y = y.with_hbbs(&y_bboxes);
-                }
+                let (y_hbbs, y_masks): (Vec<_>, Vec<_>) = y_bboxes_masks.into_iter().unzip();
+                let y = Y::default()
+                    .with_hbbs(&y_hbbs)
+                    .with_masks(&y_masks.into_iter().flatten().collect::<Vec<_>>());
 
                 Some(y)
             })
