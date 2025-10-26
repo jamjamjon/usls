bba7512bcaf737a0f295117c507f372878603d9a
jamjamjon <jamjamjon@jamjamjondeMac-mini.local>
2025-10-27 00:11:17 +0800

    Add RF-DETR-seg model

--- examples/rfdetr/main.rs
+++ examples/rfdetr/main.rs
@@ -1,14 +1,50 @@
 use anyhow::Result;
-use usls::{models::RFDETR, Annotator, Config, DataLoader};
+use usls::{models::RFDETR, Annotator, Config, DataLoader, Scale, Task};
+
+#[derive(argh::FromArgs)]
+/// Example
+struct Args {
+    /// device
+    #[argh(option, default = "String::from(\"cpu\")")]
+    device: String,
+
+    /// task
+    #[argh(option, default = "String::from(\"det\")")]
+    task: String,
+
+    /// scale
+    #[argh(option, default = "String::from(\"n\")")]
+    scale: String,
+
+    /// dtype
+    #[argh(option, default = "String::from(\"auto\")")]
+    dtype: String,
+}
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
         .init();
+    let args: Args = argh::from_env();
 
     // config
-    let mut model = RFDETR::new(Config::rfdetr_base().commit()?)?;
+    let config = match args.task.parse()? {
+        Task::ObjectDetection => match args.scale.parse()? {
+            Scale::N => Config::rfdetr_nano(),
+            Scale::S => Config::rfdetr_small(),
+            Scale::M => Config::rfdetr_medium(),
+            Scale::B => Config::rfdetr_base(),
+            Scale::L => Config::rfdetr_large(),
+            _ => unimplemented!("Unsupported model scale: {:?}. Try b, s, t.", args.scale),
+        },
+        Task::InstanceSegmentation => Config::rfdetr_seg_preview(),
+        _ => unimplemented!("Unsupported task: {:?}. Try det, seg.", args.task),
+    }
+    .with_dtype_all(args.dtype.parse()?)
+    .with_device_all(args.device.parse()?)
+    .commit()?;
+    let mut model = RFDETR::new(config)?;
 
     // load
     let xs = DataLoader::try_read_n(&["./assets/bus.jpg"])?;
